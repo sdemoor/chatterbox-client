f86fd311ff317c98ba43648dbb00a201764f46db
Simon de Moor <simondemoor0@gmail.com>
2017-02-20 17:08:04 -0800

    First stages in place, message input.

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -3,6 +3,6 @@
 
 // Put your parse application keys here!
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
+  jqXHR.setRequestHeader('X-Parse-Application-Id', 'wPfavNOxOKTzlXiMq9r226nSo5Vl5aMFfEcvAupE');
+  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'eCGqXWtDjoZofGzf785KvSODrUrXURbL6JCQqaHj');
 });
