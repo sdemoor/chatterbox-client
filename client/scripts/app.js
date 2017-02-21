08ffcc31f962f83cbe1d8d52b95506107d853441
Simon de Moor <simondemoor0@gmail.com>
2017-02-20 17:30:01 -0800

    Sending message works !

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,35 +1,39 @@
 
-var message = {
-  username: 'username',
-  text: 'Hello World!!!',
-  roomname: 'hrr22 4life'
-};
+// var message = {
+//   username: 'username',
+//   text: 'Hello World!!!',
+//   roomname: 'hrr22 4life'
+// };
 
 var app = {};
 
 app.init = function() {};
-app.send = function()  {
+app.send = function(message)  {
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
+    url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
 };
-app.send();
 
 $(document).ready(function() {
   $( ".btn" ).click(function(event) {
     var visitorMessage = $( ".entry" ).val();
-    console.log(window.location.href.split('=')[1]);
-    return visitorMessage;
+    console.log(visitorMessage);
+    var user = window.location.href.split('username=')[1];
+    var message = {};
+    message.username = user;
+    message.text = visitorMessage;
+    message.roomname = 'test';
+    app.send(message);
   });
 });
