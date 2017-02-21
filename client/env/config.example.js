b7c113a797405fca757b2b7fbc7cfb87f453bd23
Simon de Moor <simondemoor0@gmail.com>
2017-02-21 11:06:45 -0800

    All tests pass but last 2

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -3,6 +3,6 @@
 
 // Put your parse application keys here!
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', 'wPfavNOxOKTzlXiMq9r226nSo5Vl5aMFfEcvAupE');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'eCGqXWtDjoZofGzf785KvSODrUrXURbL6JCQqaHj');
+  jqXHR.setRequestHeader('X-Parse-Application-Id', '');
+  jqXHR.setRequestHeader('X-Parse-REST-API-Key', '');
 });
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -11,7 +11,7 @@ app.init = function() {};
 app.send = function(message)  {
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+    url: this.server,
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
@@ -25,15 +25,73 @@ app.send = function(message)  {
   });
 };
 
+app.fetch = function() {
+  $.get(this.server,function(messages) {
+    console.log('messages came in');
+    console.log(messages);
+    var messages = messages.results;
+    messages.forEach(function(message) {
+      // var text = jsesc(message.text);
+      app.renderMessage(message);
+    });
+  });
+};
+
+app.server = 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages';
+app.room = 'lobby';
+app.fetch();
+
 $(document).ready(function() {
-  $( ".btn" ).click(function(event) {
+  $( ".submit" ).click(function(event) {
     var visitorMessage = $( ".entry" ).val();
     console.log(visitorMessage);
     var user = window.location.href.split('username=')[1];
     var message = {};
     message.username = user;
     message.text = visitorMessage;
-    message.roomname = 'test';
+    message.roomname = app.room;
     app.send(message);
   });
+
+  $( ".clear" ).click(function(event) {
+    app.clearMessages();
+  });
+
+  $("#roomSelect").change(function(event) {
+    if(this.value === 'add-room') {
+      var roomAdd = prompt('What is your room name?');
+      app.renderRoom(roomAdd);
+    } else {
+      app.clearMessages();
+      app.room = this.value;
+      app.fetch();
+    }
+  });
 });
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.renderMessage = function(message) {
+  if(app.room === message.roomname) {
+    var messageContainer = $('<div class="chat"></div>');
+    messageContainer.append('<div class="username">' + message.username + '</div>');
+    messageContainer.append('<div class="message-text">' + message.text + '</div');
+    $('#chats').append(messageContainer);
+  }
+  //return messageContainer;
+};
+app.renderRoom = function(roomName) {
+
+  if(roomName) {
+    var newRoomMod = roomName.replace(' ', '-');
+    $("#roomSelect").prepend($('<option value="' + newRoomMod + '">' + roomName + '</option>'));
+    $("#roomSelect").val(newRoomMod);
+    app.room = newRoomMod;
+    app.clearMessages();
+    app.fetch();
+  }
+};
+
+//app.fetch();
